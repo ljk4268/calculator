8acbd4f8271f4320b2899471a181de264ff21e9a
ljk4268 <ljk4268@kakao.com>
2022-04-24 00:13:34 +0900

    계산기 에러 수정
    - 연산자 중복으로 눌렀을 때 알림창 뜨지 않는거 고침
    - 숫자 '0'을 쓰면 '0'이 빈칸으로 바뀌어서 '0'을 못 쓰는 문제를 해결.
    - checkOp를 true로 바꾸면서 && 35번째 줄을 지우면서 해결.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let buttons = document.querySelectorAll('button');
 let $operator = document.querySelectorAll('.operator');
 let operators = '';
 let firstNum = 0;
-let checkOp = false;
+let checkOp = true;
+      // 다음숫자를 입력하기 위해서 checkOp를 true로 해둠.
+
 // 실제 필요한 값이 아니더라도 변수를 선언할 때는 초기화를 해준다. 
 
 // 연산을 위한 함수
@@ -11,7 +13,7 @@ function operating(firstNum, op, secondNum) {
   if (op === '') {
     return secondNum
   }
-  // 연산을 하고 연산한 결과를 리턴해줘야함. (2 + 2)
+  // 연산을 하고 연산한 결과를 리턴해줘야함. 
   else {
     firstNum = Number(firstNum);
     secondNum = Number(secondNum);
@@ -30,21 +32,23 @@ function operating(firstNum, op, secondNum) {
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (display.innerText === '0') {
+    // if (display.innerText === '0') {
+    //   display.innerText = '';
+    // }
+    if (checkOp === true) {
       display.innerText = '';
+      checkOp = false;
     }
     if (button.className === 'number') {
-      if (checkOp === true) {
-        display.innerText = '';
-        checkOp = false;
-      }
+      // 다음숫자를 입력하기 위해서 checkOp를 true로 해둠. - 비교를 위한 부분. 
       display.innerText += button.innerText;
     }
     if (button.className === 'operator') {
-      checkOp = true;
+      checkOp = true; 
       // 1. 입력한 값이 없을 때
-      if (display.innerText === '') {
+      if (display.innerText === '' && operators === '') {
         alert('숫자를 입력해주세요 ><!');
+        display.innerText = '0';
       } else {
         // 2. 입력한 값이 있을 때
         // 2-1. 숫자를 처음 누르고 연산자를 눌렀을 경우. (ex: 2 + ) -> 
@@ -60,7 +64,7 @@ buttons.forEach((button) => {
     if (button.className === 'ac') {
       operators = '';
       firstNum = 0;
-      checkOp = false;
+      checkOp = true;
       display.innerText = '0';
     }
   })
